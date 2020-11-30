3688246453f803203026405643c9b03d6e27558c
HannaChae <hannachae.dev@gmail.com>
2020-11-30 18:00:30 +0900

    18:00

---
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.demo.domains.UserDto;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @PostMapping("/users")
+    public Map<String, String> join(@RequestBody UserDto user) {
+        System.out.println("===== Join =====");
+        Map<String, String> map = new HashMap<>();
+        System.out.println("아이디: " + user.getUserId());
+        System.out.println("비밀번호: " + user.getPassword());
+        System.out.println("이름: " + user.getName());
+        map.put("name", user.getName());
+        return map;
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/impls/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.impls;
+
+import com.example.demo.domains.UserDto;
+import com.example.demo.services.UserService;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void joinUser(UserDto userDto) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
